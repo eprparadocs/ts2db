0120bbfe81337074da2f24dc6f13d98e95484ff1
Charles Ginger Wegrzyn <ts2db@garbagedump.com>
2022-11-25 07:24:35 -0500

    Beginning of SQL specific information for AST

---
+++ Python/src/Sql2GraphQL/sqlbits.h
@@ -0,0 +1,10 @@
+// Selectops bit settings
+#define  SELECTOPS_INIT         00	
+#define  SELECTOPS_ALL	        01
+#define  SELECTOPS_DISTINCY     02
+#define  SELECTOPS_DISTROW      04
+#define  SELECTOPS_HPRI        010
+#define  SELECTOPS_SJOIN       020
+#define  SELECTOPS_SMALLR      040
+#define  SELECTOPS_BIGRST     0100
+#define  SELECTOPS_CFROWS     0200
