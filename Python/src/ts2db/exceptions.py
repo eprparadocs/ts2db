82c4815fe21fac1da992b7b0eb30ac54fbaae239
Charles Ginger Wegrzyn <ts2db@garbagedump.com>
2022-12-11 14:20:26 -0500

    Added some simple comments

--- Python/src/ts2db/exceptions.py
+++ Python/src/ts2db/exceptions.py
@@ -1,8 +1,17 @@
 class TableError(Exception):
+    """
+    A table level error.
+    """
     pass
 
 class NameError(Exception):
+    """
+    Column name error.
+    """
     pass
 
 class TypeError(Exception):
+    """
+    Column type error.
+    """
     pass
